210f9eea61e265951377669874ba0d210cb0f6d2
HotsixMan <hotsixman05@gmail.com>
2024-09-12 21:01:14 +0900

    ./

--- cjs/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { runQuery } from "./module/runQuery.js";
-import { QueryCallback } from "./types";
-export declare function defineDBHandler<T extends any[], K = any>(handler: (...args: T) => QueryCallback<K>): ((...args: T) => Promise<K>) & {
-    getCallback: (...args: T) => QueryCallback<K>;
-};
-export { runQuery };
-//# sourceMappingURL=index.d.ts.map
--- cjs/types.d.ts
+++
@@ -1,3 +0,0 @@
-export type QueryCallback<T = any> = (queryFunction: QueryFunction) => Promise<T>;
-export type QueryFunction<T = any> = (query: string, values?: any[]) => Promise<T>;
-//# sourceMappingURL=types.d.ts.map
