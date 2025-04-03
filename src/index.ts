2482339fbc19cbab3289e11b64d93da28162f505
HotsixMan <hotsixman05@gmail.com>
2025-04-03 14:06:50 +0900

    build: 0.5.1

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { defaultDBConnector } from './module/defaultDBConnector.js';
 import { QueryCallback, RunQueryMode } from './types.js';
 
-export const defineDBHandler = function<T extends any[], K = any>(handlers: (...args: T) => QueryCallback<K>){
+export const defineDBHandler = function <T extends any[], K = any>(handlers: (...args: T) => QueryCallback<K>) {
     return defaultDBConnector.defineDBHandler(handlers);
 };
-export const runQuery = async function<T = any>(callback: QueryCallback<T>, mode: RunQueryMode = 'conn'){
+export const runQuery = async function <T = any>(callback: QueryCallback<T>, mode: RunQueryMode = 'conn') {
     return await defaultDBConnector.runQuery(callback, mode);
 };
 
 export { DBConnector } from './module/DBConnector.js';
 export { defaultDBConnector };
+
+import queryBuilder from './module/queryBuilder.js';
+export {queryBuilder}
+export * from './module/queryBuilder.js';
